4b76ae56b946adab8f1d52fbbfb289ab0d5a0338
vetrivel s <vetrivels@vetrivel-s-2.local>
2026-02-09 11:08:48 +0530

    Initial commit

---
+++ src/main/java/com/ErpsEtErp0005Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpsEtErp0005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpsEtErp0005Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ErpsEtErp0005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpsEtErp0005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
